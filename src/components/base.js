53f8cfae6b8c975bc6061feea14decd3cebccbea
Mayur Agarwal <65847550+mayuragarwal2004@users.noreply.github.com>
2023-04-21 12:21:23 +0530

    Update base.js

--- src/components/base.js
+++ src/components/base.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyB1tevf2sUEWfqRoi9Rw5rNuEEHHtfszbc",
+  authDomain: "chatlux-4f186.firebaseapp.com",
+  projectId: "chatlux-4f186",
+  storageBucket: "chatlux-4f186.appspot.com",
+  messagingSenderId: "1010598671959",
+  appId: "1:1010598671959:web:621eb7d5999c2104b91512"
 };
 
 // Initialize Firebase
